e4263ebf65ae72244caab2028dda9fbe83d0d188
Sascha Majewsky <sascha.majewsky@pm.me>
2021-06-23 16:01:49 +0200

    Refactored custom email template

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Providers from "next-auth/providers";
+import customVerificationRequest from "./customVerificationRequest";
 
 export default NextAuth({
   providers: [
