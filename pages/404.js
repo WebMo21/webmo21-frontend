bca2bedcd827f1700c75e0c1f4b9b9767ccf9304
Sascha Majewsky <sascha.majewsky@pm.me>
2021-06-23 15:28:58 +0200

    Added custom 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 
-const error = () => {
+const Custom404 = () => {
   return (
     <div className="!h-screen">
       <div className="flex-auto ct-docs-frame">
@@ -10,17 +10,22 @@ const error = () => {
             <div className="h-screen overflow-hidden">
               <div
                 className="absolute w-full h-full bg-cover bg-50"
-                style={{ backgroundImage: "url(./backgrounds/error.svg)" }}
+                style={{
+                  backgroundImage:
+                    "url(./backgrounds/bg-fitness-man-dark.jpeg)",
+                }}
               ></div>
               <div className="container h-full px-4 mx-auto">
                 <div className="flex flex-wrap items-center h-full -mx-4">
                   <div className="relative w-full px-4 text-center">
                     <h1 className="font-bold text-green-500 text-10 sm:text-12 tracking-875">
-                      500
+                      404
                     </h1>
-                    <h4 className="mt-0 mb-2 text-3xl font-bold">Fehler :(</h4>
-                    <p className="text-2xl font-normal leading-normal text-blueGray-500">
-                      Da haben wir uns wohl verhoben...
+                    <h4 className="mt-0 mb-2 text-3xl font-bold text-white">
+                      Fehler :(
+                    </h4>
+                    <p className="text-2xl font-normal leading-normal text-white">
+                      Dieses Training ist uns neu...
                     </p>
                     <Link href="/dashboard">
                       <a className="inline-block p-4 mt-5 font-semibold text-white bg-green-500 border border-green-500 rounded cursor-pointer select-none hover:bg-green-400">
@@ -38,4 +43,4 @@ const error = () => {
   );
 };
 
-export default error;
+export default Custom404;
